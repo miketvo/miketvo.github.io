e8acfa6636bf67922299d7fbf65dc1710a051a58
miketvo <ikxplicitie@gmail.com>
2020-12-13 03:12:17 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Scroll to top button
+
+scrollTopButton = document.getElementById("bttn-backtotop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopButton.style.display = "initial";
+    } else {
+        scrollTopButton.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
